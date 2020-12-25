05d138ee27abe2efee136041ff23d8a62a307d02
AnuraagGRao <anuraag.g.rao@gmail.com>
2020-12-25 15:48:09 +0530

    hotfix

--- nb/settings.py
+++ nb/settings.py
@@ -1,12 +1,5 @@
 #settings
 
-import os
-from pathlib import Path
-from dotenv import load_dotenv
-
-env_path = Path('.') / '.env'
-load_dotenv(dotenv_path=env_path)
-
 class Config():
 
     DEBUG = False
