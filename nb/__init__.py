d9c7abfa35c4214632e959edcb2bc66c37b67ec6
AnuraagGRao <anuraag.g.rao@gmail.com>
2020-12-25 15:23:03 +0530

    ec2 nginx

--- nb/__init__.py
+++ nb/__init__.py
@@ -11,3 +11,8 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 
 from nb import routes
+
+if __name__ == "__main__":
+    from gevent.pywsgi import WSGIServer
+    http_server = WSGIServer(('', 8000), app)
+    http_server.serve_forever()
